3ab6c0672a906b26ed75aa0995f86c1e5fddd772
juandrzej <ziebandrzej@gmail.com>
2025-02-22 13:52:01 +0100

    refactoring

--- 100days_of_code_day45_100movies/main.py
+++ 100days_of_code_day45_100movies/main.py
@@ -1,25 +1,41 @@
+import bs4
 import requests
-from bs4 import BeautifulSoup
-
-# URL of the webpage to scrape
-URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
-
-# Send a GET request to the URL
-response = requests.get(URL)
-response.raise_for_status()  # Ensures any HTTP errors are raised
-
-# Parse the webpage content
-soup = BeautifulSoup(response.text, "html.parser")
-
-# Extract movie titles from the webpage
-movie_titles_web = soup.find_all(name="h3", class_="title")
-movie_titles = [title.getText() for title in movie_titles_web[::-1]]
-# movie_titles = [title.getText() for title in reversed(movie_titles_web)]
+from typing import List
 
 
-# Save the extracted movie titles to a file
-with open('movies.txt', 'w', encoding="utf-8") as file:
-    for line in movie_titles:
-        file.write(f"{line}\n")
+def fetch_webpage(url: str) -> str:
+    """Fetches the content of a webpage."""
+    response = requests.get(url)
+    response.raise_for_status()  # Raises an HTTPError for bad responses
+    return response.text
 
-print(f"Successfully saved {len(movie_titles)} movie titles to 'movies.txt'.")
+
+def parse_movie_titles(html: str) -> List[str]:
+    """Parses the HTML content and extracts movie titles."""
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    movie_title_elements = soup.find_all(name="h3", class_="title")
+    return [title.get_text() for title in reversed(movie_title_elements)] # alternative [::-1]
+
+
+def save_to_file(movie_titles: List[str], filename: str) -> None:
+    """Saves a list of movie titles to a file."""
+    with open(filename, 'w', encoding="utf-8") as file:
+        for title in movie_titles:
+            file.write(f"{title}\n")
+
+
+def main() -> None:
+    url: str = "https://web.archive.org/web/20200518073854/https://www.empireonline.com/movies/features/best-movies-2/"
+
+    # Fetch and parse the webpage
+    webpage_content = fetch_webpage(url)
+    movie_titles = parse_movie_titles(webpage_content)
+
+    # Save the movie titles to a file
+    save_to_file(movie_titles, 'movies.txt')
+
+    print(f"Successfully saved {len(movie_titles)} movie titles to 'movies.txt'.")
+
+
+if __name__ == '__main__':
+    main()
