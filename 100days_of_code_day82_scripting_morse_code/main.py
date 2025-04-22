7519fb15a492c572210255f4878c5a20b4c4ec2d
juandrzej <programming.yippee729@passinbox.com>
2025-04-22 21:57:36 +0200

    starting day 82

---
+++ 100days_of_code_day82_scripting_morse_code/main.py
@@ -0,0 +1,11 @@
+def converter(base_list: list[str]) -> str:
+    for char in base_list:
+        pass
+
+def main():
+    user_input = input("Please give me a string to convert into Morse Code: ")
+    user_input_list = list(user_input)
+    print(user_input_list)
+
+if __name__ == "__main__":
+    main()
