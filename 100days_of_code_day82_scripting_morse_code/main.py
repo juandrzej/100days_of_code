9b6d895237f529cfad05c009db2f66c291f1219f
juandrzej <programming.yippee729@passinbox.com>
2025-04-25 21:42:53 +0200

    some actual progress made

--- 100days_of_code_day82_scripting_morse_code/main.py
+++ 100days_of_code_day82_scripting_morse_code/main.py
@@ -14,16 +14,23 @@ morse_code: dict[str, str] = {
     "m": "___ ___",
     "n": "___ .",
     "o": "___ ___ ___",
+    "p": ". ___ ___ .",
 }
 
 def converter(base_list: list[str]) -> str:
+    converted_list: list = []
     for char in base_list:
-        pass
+        if char in morse_code:
+            converted_list.append(morse_code[char])
+    
+    return "".join(converted_list)
 
 def main():
     user_input = input("Please give me a string to convert into Morse Code: ")
     user_input_list = list(user_input)
     print(user_input_list)
+    converted_input: str = converter(user_input_list)
+    print(converted_input)
 
 if __name__ == "__main__":
     main()
