637f4c30c1d00a097293cac49f864b27387d3107
juandrzej <programming.yippee729@passinbox.com>
2025-06-01 22:42:08 +0200

    visualization

--- day082_to_day100_projects/day84_tic_tac_toe/main.py
+++ day082_to_day100_projects/day84_tic_tac_toe/main.py
@@ -1,5 +1,22 @@
+import os
+
+
 def main() -> None:
-    pass
+    draw_board()
+
+
+def draw_board() -> None:
+    """Function to draw the game board."""
+    width = os.get_terminal_size().columns
+    print(
+        """
+       | O |   
+    -----------
+     X | O | X
+    -----------
+       | X |
+    """.center(width)
+    )
 
 
 if __name__ == "__main__":
